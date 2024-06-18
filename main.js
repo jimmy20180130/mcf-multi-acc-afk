2fdfe8d72009428e768b2baaa930e25e7e2efd2c
XiaoXi_TW <68285469+jimmy20180130@users.noreply.github.com>
2024-06-19 02:05:33 +0800

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,8 @@ const botArgs = {
     host: 'sg.mcfallout.net',
     port: '25565',
     version: '1.20.1',
-    auth: 'microsoft'
+    auth: 'microsoft',
+    physicsEnabled: true
 }
 
 const initBot = () => {
@@ -36,17 +37,17 @@ const initBot = () => {
             try {
                 if (config.trade_text && config.trade_text !== '') bot.chat(`$${config.trade_text}`)
                 if (config.lottery_text && config.lottery_text !== '') bot.chat(`%${config.lottery_text}`)
-            } catch {}
+            } catch { }
         }, 605000)
 
         facility = setInterval(function () {
             config = JSON.parse(fs.readFileSync(`${process.cwd()}/config/config.json`, 'utf8'))
-            try { if (config.facility_text && config.facility_text !== '') bot.chat(`!${config.facility_text}`) } catch {}
+            try { if (config.facility_text && config.facility_text !== '') bot.chat(`!${config.facility_text}`) } catch { }
         }, 1805000)
 
         auto_warp = setInterval(function () {
             config = JSON.parse(fs.readFileSync(`${process.cwd()}/config/config.json`, 'utf8'))
-            try { bot.chat(config.warp) } catch {}
+            try { bot.chat(config.warp) } catch { }
         }, 600000)
     }
 
@@ -60,7 +61,7 @@ const initBot = () => {
 
         console.log(`正在登入 ${botArgs.username}`);
     })
-    
+
 
     bot.once("spawn", () => {
         bot.chat('bot is online')
@@ -75,7 +76,7 @@ const initBot = () => {
             if (config.trade_text && config.trade_text !== '') bot.chat(`$${config.trade_text}`)
             if (config.lottery_text && config.lottery_text !== '') bot.chat(`%${config.lottery_text}`)
             if (config.facility_text && config.facility_text !== '') bot.chat(`!${config.facility_text}`)
-        } catch {}
+        } catch { }
 
         setTimeout(() => {
             if (botArgs.username == config.accounts[0].username) {
@@ -130,11 +131,11 @@ const initBot = () => {
     bot.on('autoeat_started', (item, offhand) => {
         console.log(`正在吃 ${offhand ? '副手中' : '主手中'} 的 ${item.name}`)
     })
-    
+
     bot.on('autoeat_finished', (item, offhand) => {
         console.log(`已吃完 ${offhand ? '副手中' : '主手中'} 的 ${item.name}`)
     })
-    
+
     bot.on('autoeat_error', error => {
         if (error.message == 'No food found.') {
             console.log('找不到食物')
@@ -142,6 +143,45 @@ const initBot = () => {
             console.log(`吃東西時發生錯誤: ${error.message}`)
         }
     })
+
+    bot.on('physicsTick', () => {
+        for (const entity of Object.values(bot.entities)) {
+            if (entity === bot.entity) { continue }
+            applyEntityCollision(entity)
+        }
+
+        //check if the bot is in the water
+        if (bot.blockAt(bot.entity.position).type === 32) {
+            console.log('Bot is in the water')
+            //檢查水的流向
+            let flow = bot.blockAt(bot.entity.position).metadata
+            console.log('Flow: ' + flow)
+        }
+    })
+
+    function applyEntityCollision(other) {
+        let dx = other.position.x - bot.entity.position.x;
+        let dy = other.position.y - bot.entity.position.y;
+        let dz = other.position.z - bot.entity.position.z;
+        let largestDistance = Math.max(Math.abs(dx), Math.abs(dz));
+        if (largestDistance >= 0.01) {
+            let vx = dx / 20;
+            let vz = dz / 20;
+
+            if (largestDistance < 1) {
+                vx /= Math.sqrt(largestDistance)
+                vz /= Math.sqrt(largestDistance)
+            } else {
+                vx /= largestDistance
+                vz /= largestDistance
+            }
+            bot.entity.xVelocity -= vx;
+            bot.entity.xVelocity -= vz;
+
+            other.xVelocity += vx;
+            other.zVelocity += vz;
+        }
+    }
 }
 
 initBot();
